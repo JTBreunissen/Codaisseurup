b0118b3d2cc49d1af85f2b4a432c61b2b7527a90
JTBreunissen <jantjarco@gmail.com>
2017-03-14 16:26:43 +0100

    Created new event view and form

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :description, precense: true, length: {maximum: 500}
-  validates: :starts_at, precense: true
-  validates: :ends_at, precense: true
-  validates_date: :ends_at :on_or_before => :start_at, :on_or_before_message => "End date must be at least 1 day after start date"
+  validates :description, presence: true, length: {maximum: 500}
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+user1 = User.create ( { email: "jantjarco@codaisseurup.com", password: "fakepassword"})
+
+event1 = Event.create ( {name:"PartyHardy", description:"Party harder", location:"Amsterdam", capacity: 50, includes_food: false, includes_drinks: true, starts_at: 10.days.from_now, ends_at: 12.days.from_now, active:true, user: user1 })
+event2 = Event.create ( {name:"LoungeDown", description:"Get mellow", location:"Utrecht", capacity: 20, includes_food: false, includes_drinks: true, starts_at: 10.days.from_now, ends_at: 12.days.from_now, active:true, user: user1 })
+event2 = Event.create ( {name:"WrongStartDate", description:"Too bad", location:"Den Haag", capacity: 20, includes_food: false, includes_drinks: true, starts_at: 10.days.from_now, ends_at: 12.days.from_now, active:true, user: user1 })
