2fa6c7a0a7daad18a3b7e607a8adf925b590d690
JTBreunissen <jantjarco@gmail.com>
2017-03-14 11:06:22 +0100

    added default values to price, food, drinks and active

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, precense: true, length: {maximum: 500}
+  validates: :starts_at, precense: true
+  validates: :ends_at, precense: true
+  validates_date: :ends_at :on_or_before => :start_at, :on_or_before_message => "End date must be at least 1 day after start date"
+
 end
